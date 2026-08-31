eccecacbb9f711d62d94934c45a65d3828d486a2
AlexWybranski <alexwybranski222@gmail.com>
2026-08-31 17:58:09 +0200

    [#6] Control packet update

--- shared/packet.hpp
+++ shared/packet.hpp
@@ -8,9 +8,9 @@ namespace Buttons {
     constexpr uint8_t UP = 2U;
     constexpr uint8_t DOWN = 3U;
     constexpr uint8_t CROSS = 4U;
-    constexpr uint8_t SQUARE = 5U;
-    constexpr uint8_t TRIANGLE = 6U;
-    constexpr uint8_t CIRCLE = 7U;
+    constexpr uint8_t Res0 = 5U;
+    constexpr uint8_t Res1 = 6U;
+    constexpr uint8_t NO_PAD = 7U;
 }
 
 struct __attribute__((packed)) DroneControlPacket {
